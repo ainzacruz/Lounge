1983351433f0915b6fb9958c75b0a9ec29b596a6
Adrian Inza-Cruz <ainzacruz@gmail.com>
2019-03-15 11:53:41 -0700

    seeding database for testing

---
+++ src/db/seeds/20190315185202-topics.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const faker = require("faker");
+
+//#2
+let topics = [];
+
+for (let i = 1; i <= 15; i++) {
+  topics.push({
+    title: faker.hacker.noun(),
+    description: faker.hacker.phrase(),
+    createdAt: new Date(),
+    updatedAt: new Date()
+  });
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert("Topics", topics, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete("Topics", null, {});
+  }
+};
