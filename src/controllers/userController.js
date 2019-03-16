e4a78883637f91093cd2104458c95afd5f10e1c3
Adrian Inza-Cruz <ainzacruz@gmail.com>
2019-03-16 15:07:34 -0700

    sign out

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -39,5 +39,10 @@ module.exports = {
         res.redirect("/");
       }
     });
+  },
+  signOut(req, res, next) {
+    req.logout();
+    req.flash("notice", "You've successfully signed out!");
+    res.redirect("/");
   }
 };
